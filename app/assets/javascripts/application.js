edf709cbc523c57d4e6d8b3a5b3159199d17382a
Dawid Szpener <dawidszpener141@gmail.com>
2021-02-12 16:24:56 +0000

    different navbar elements displayed for different users

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs 
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
+//= require turbolinks
 //= require bootstrap
 //= require bootstrap.min
 //= require bootstrap-sprockets
