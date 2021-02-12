33911b3871016d1bec5207a7cd76cba8f216bf9d
Dawid Szpener <dawidszpener141@gmail.com>
2021-02-12 13:43:10 +0000

    create admin dashboard permissions

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -46,6 +46,8 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  config.assets.check_precompiled_asset = false
+
   # Raise exceptions for disallowed deprecations.
   config.active_support.disallowed_deprecation = :raise
 
