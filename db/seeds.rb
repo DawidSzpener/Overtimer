9cd430507acae624be1e3ff31091d854dd6c58b3
Dawid Szpener <dawidszpener141@gmail.com>
2021-02-12 21:47:49 +0000

    updated views to not see edit button if post is approved

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,17 @@ puts "1 user created"
 AdminUser.create(email: "admintest@test.com", password: "asdasd", password_confirmation: "asdasd", first_name: "Dawid", last_name: "Szef")
 puts "1 admin user created"
 
-50.times do |post|
-  Post.create(date: Date.today, rationale: "#{post} rationale content here", user_id: @user.id)
+10.times do |post|
+  Post.create(date: Date.today, rationale: "#{post} rationale content here", user_id: @user.id, status: "submitted")
 end
+puts "10 posts have been created"
 
-puts "50 posts have been created"
+10.times do |post|
+  Post.create(date: Date.today, rationale: "#{post} rationale content here", user_id: @user.id, status: "approved")
+end
+puts "10 approved posts have been created"
+
+10.times do |post|
+  Post.create(date: Date.today, rationale: "#{post} rationale content here", user_id: @user.id, status: "rejected")
+end
+puts "10 rejected posts have been created"
