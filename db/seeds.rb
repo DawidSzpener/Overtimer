75973ab1dcfcab3e6a3cdfca5392b53f20834820
Dawid Szpener <dawidszpener141@gmail.com>
2021-02-13 20:40:09 +0000

    integreted callback for rejections

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,11 @@ puts "1 admin user created"
 @user = User.create(email: "test@test.com", password: "asdasd", password_confirmation: "asdasd", first_name: "Dawid", last_name: "User")
 puts "1 user created"
 
+AuditLog.create(start_date: (Date.today - 6.days), user_id: User.last.id, status: 0)
+AuditLog.create(start_date: (Date.today - 13.days), user_id: User.last.id, status: 0)
+AuditLog.create(start_date: (Date.today - 20.days), user_id: User.last.id, status: 0)
+puts "3 audit logs have been created"
+
 10.times do |post|
   Post.create(date: Date.today, rationale: "#{post} rationale content here", user_id: @user.id, status: "submitted", overtime_request: 2.5)
 end
@@ -18,8 +23,3 @@ puts "10 approved posts have been created"
   Post.create(date: Date.today, rationale: "#{post} rationale content here", user_id: @user.id, status: "rejected", overtime_request: 2.5)
 end
 puts "10 rejected posts have been created"
-
-AuditLog.create(start_date: (Date.today - 6.days), user_id: User.last.id, status: 0)
-AuditLog.create(start_date: (Date.today - 13.days), user_id: User.last.id, status: 0)
-AuditLog.create(start_date: (Date.today - 20.days), user_id: User.last.id, status: 0)
-puts "3 audit logs have been created"
